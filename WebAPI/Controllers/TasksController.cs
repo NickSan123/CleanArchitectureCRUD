c600bd5f81da73ac0990c8b3a4a57fc99ec8757f
NicolaiTeleco <nicolai.martins@online.net.br>
2024-11-19 18:02:51 -0300

    Criado Task Controllers

---
+++ WebAPI/Controllers/TasksController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Infrastructure.Data;
+using Domain.Interfaces;
+using Task = Domain.Entities.Task;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskRepository _repository;
+
+        public TasksController(ITaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _repository.GetAllAsync());
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTask(int id)
+        {
+           return Ok(await _repository.GetByIdAsync(id));
+        }
+
+        // PUT: api/Tasks/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<IActionResult> PutTask(Task task)
+        {
+            if (task.Id == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_repository.CreateAsync(task));
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            return Ok(_repository.DeleteByIdAsync(id));
+        }
+    }
+}
--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UserController : Controller
     {
         private readonly IUserRepository _userRepository;
